fedcda9f430b3170dcf8aa1dd2d4b0a55f71ce12
DilyaraSuleimanova <suleimanovadilyara04@gmail.com>
2026-08-20 23:35:25 +0500

    Image gallery with flex

---
+++ 9. Flex panels_JS_5/script.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    panels.forEach(panel => {
+        if(panel.classList.contains('open') && !this.classList.contains('open')) {
+            panel.classList.remove('open');
+        }
+    });
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    if(e.propertyName.includes('flex')){
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
